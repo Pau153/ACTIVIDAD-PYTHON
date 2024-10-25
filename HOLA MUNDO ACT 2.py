39bc0b2fabd4ffd3c64b8dfa66b6fffd37b71ff8
pau153 <paulamh@jarandes.com>
2024-10-25 14:02:33 -0500

    actualizacion

---
+++ HOLA MUNDO ACT 2.py
@@ -0,0 +1,18 @@
+print ("hola mundo")
+
+#Variable
+edad_usuario = 22
+nombre_usuario = "paula"
+print (edad_usuario)
+print (nombre_usuario)
+
+#entero
+numero = 10+1 
+
+ #boleano 
+es_estudiante = True 
+
+
+#otros datos 
+
+
